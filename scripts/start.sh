084075b87c7152b455bbf4089ab85f4cf16c979e
BhukyAkash <118367712+BhukyAkash@users.noreply.github.com>
2024-12-16 12:29:31 +0530

    Delete scripts directory

--- scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/springboot
-nohup java -jar myapp.jar > /dev/null 2>&1 &
--- scripts/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pid=$(ps -ef | grep 'myapp.jar' | grep -v grep | awk '{print $2}')
-if [ -z "$pid" ]; then
-    echo "No process found"
-else
-    kill -9 $pid
-    echo "Stopped the Spring Boot app"
-fi
